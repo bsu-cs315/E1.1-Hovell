ddb2f7a7ec67f83b35c7ad4e45fb252de0602950
Camden Hovell <camden.hovell@bsu.edu>
2024-08-31 15:45:07 -0400

    Add player control of shooter's direction

--- shooter.gd
+++ shooter.gd
@@ -1,15 +1,28 @@
 extends RigidBody2D
 
 var strength : float = 20
+var yDirection : float = 0
+var isShot : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	gravity_scale = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("launch"):
-		var direction = Vector2(1, -1).normalized()
+	if Input.is_action_just_pressed("launch") && !isShot:
+		var direction = Vector2(1, yDirection).normalized()
 		var velocity = direction * strength
-		apply_impulse(Vector2(50,0), velocity)
+		apply_impulse(Vector2(50,yDirection), velocity)
+		gravity_scale = 0.1
+		isShot = true
+		
+	if Input.is_action_just_pressed("addY") && yDirection < 90 && !isShot:
+		yDirection += 10
+		$"../Arrow".rotation_degrees += 10
+		
+	if Input.is_action_just_pressed("subtractY") && yDirection > -90 && !isShot:
+		yDirection -= 10
+		$"../Arrow".rotation_degrees -= 10
+	
