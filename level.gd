27adb23e92b4a799d3d8545af46c9e954299f6d1
Camden Hovell <camden.hovell@bsu.edu>
2024-08-31 16:48:56 -0400

    Refactor code to seperate Level and Shooter functions

--- level.gd
+++ level.gd
@@ -3,9 +3,27 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$HUD.updateStrength($Shooter.strength)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (Input.is_action_just_pressed("launch")):
+		$Shooter.launch()
+		$Arrow.hide()
+
+	if (Input.is_action_just_pressed("addStrength")):
+		$Shooter.addStrength()
+		$HUD.updateStrength($Shooter.strength)
+		
+	if (Input.is_action_just_pressed("subtractStrength")):
+		$Shooter.subtractStrength()
+		$HUD.updateStrength($Shooter.strength)
+		
+	if (Input.is_action_just_pressed("addY")):
+		$Shooter.addY()
+		if ($Shooter.yDirection < 90): $Arrow.rotation_degrees += 10
+		
+	if (Input.is_action_just_pressed("subtractY")):
+		$Shooter.subtractY()
+		if ($Shooter.yDirection > -90): $Arrow.rotation_degrees -= 10
--- shooter.gd
+++ shooter.gd
@@ -7,31 +7,25 @@ var isShot : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	gravity_scale = 0
-	$"../HUD".updateStrength(strength)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("launch") && !isShot:
-		var direction = Vector2(1, yDirection).normalized()
-		var velocity = direction * strength
-		apply_impulse(Vector2(50,yDirection), velocity)
-		gravity_scale = 0.1
+	
+func launch():
+	if (!isShot):
+		apply_impulse(Vector2(strength,yDirection), Vector2(1, yDirection).normalized())
+		gravity_scale = 0.2
 		isShot = true
-		$"../Arrow".hide()
 		
-	if Input.is_action_just_pressed("addY") && yDirection < 90 && !isShot:
+func addY():
+	if (yDirection < 90 && !isShot):
 		yDirection += 10
-		$"../Arrow".rotation_degrees += 10
-		
-	if Input.is_action_just_pressed("subtractY") && yDirection > -90 && !isShot:
+
+func subtractY():
+	if (yDirection > -90 && !isShot):
 		yDirection -= 10
-		$"../Arrow".rotation_degrees -= 10
 		
-	if Input.is_action_just_pressed("addStrength"):
-		strength += 20
-		$"../HUD".updateStrength(strength)
-		
-	if Input.is_action_just_pressed("subtractStrength") && strength > 0:
+func addStrength():
+	strength += 20
+	
+func subtractStrength():
+	if (strength > 0):
 		strength -= 20
-		$"../HUD".updateStrength(strength)
 	
